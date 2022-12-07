126473e593d923da53797da456362752b342a2f7
MathisMoreau <93580834+MathisMoreau@users.noreply.github.com>
2022-12-07 15:40:21 +0100

    Update recuperation_MAC_IP.py

--- supervision/connexion/recuperation_MAC_IP.py
+++ supervision/connexion/recuperation_MAC_IP.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-import os
-
 #Using the nmap command to scan network hosts
 cmd = 'echo etudiant | sudo -S nmap 172.20.90.* > nmap.txt'
 os.system(cmd)
@@ -15,7 +7,10 @@ liste2 = []
 MAC=[]
 IP = []
 mon_dico = {}
-
+MAC2 = ['8C:EC:4B:89:90:4F','8C:EC:4B:89:02:F0','8C:ER:4B:88:91:EC','8C:ER:4B:88:90:52','8C:ER:4B:88:90:52','8C:ER:4B:89:01:AC',
+        '8C:ER:4B:88:90:A4','8C:ER:4B:89:7E:A1','8C:ER:4B:88:90:AF','8C:ER:4B:89:7E:6F','8C:ER:4B:89:7E:E3','8C:ER:4B:89:7E:5E',
+        '8C:ER:4B:88:90:5C','8C:ER:4B:88:91:DD','8C:ER:4B:88:92:FA','8C:ER:4B:88:91:D9','8C:ER:4B:88:91:EF','8C:ER:4B:88:93:8C',
+        '8C:ER:4B:88:92:30','8C:ER:4B:88:91:D1','8C:ER:4B:88:92:34','8C:ER:4B:88:91:CF','8C:ER:4B:88:90:19','8C:ER:4B:88:91:F7']
 #Open nmap file
 g = open('nmap.txt','r')
 
@@ -28,8 +23,9 @@ g.close()
 #Retrieve all MAC addresses of the file
 for i in range(len(liste2)-30):
     if liste2[i] + liste2[i+1] + liste2[i+2] + liste2[i+3] + liste2[i+4] + liste2[i+5] + liste2[i+6] + liste2[i+7]+ liste2[i+8]+ liste2[i+9]+ liste2[i+10] + liste2[i+11] == 'MAC Address:':
-        MAC.append(liste2[i+13] + liste2[i+14]+liste2[i+15] + liste2[i+16] + liste2[i+17] + liste2[i+18] + liste2[i+19] + liste2[i+20] + liste2[i+21]+ liste2[i+22]+ liste2[i+23]+ liste2[i+24]+ liste2[i+25]+ liste2[i+26]+ liste2[i+27]+ liste2[i+28]+ liste2[i+29])
-
+        if liste2[i+13] + liste2[i+14]+liste2[i+15] + liste2[i+16] + liste2[i+17] + liste2[i+18] + liste2[i+19] + liste2[i+20] + liste2[i+21]+ liste2[i+22]+ liste2[i+23]+ liste2[i+24]+ liste2[i+25]+ liste2[i+26]+ liste2[i+27]+ liste2[i+28]+ liste2[i+29] in MAC2 :
+            MAC.append(liste2[i+13] + liste2[i+14]+liste2[i+15] + liste2[i+16] + liste2[i+17] + liste2[i+18] + liste2[i+19] + liste2[i+20] + liste2[i+21]+ liste2[i+22]+ liste2[i+23]+ liste2[i+24]+ liste2[i+25]+ liste2[i+26]+ liste2[i+27]+ liste2[i+28]+ liste2[i+29])
+    
 #Retrieve all IP addresses of the file
 for i in range(len(liste2)-10):
     if liste2[i] + liste2[i+1] + liste2[i+2] == "for":
